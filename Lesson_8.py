d519f4ceeff997d8c09bab297fa3ea5eaccbde90
Ukropchick <ukropcikkaneev@yandex.ru>
2021-11-13 16:42:56 +0300

    absolute all

--- Lesson_8.py
+++ Lesson_8.py
@@ -36,42 +36,62 @@ def roman(n: int) -> str:
 
 def russian(n: int) -> str:
     numbers = [["один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"],
-               ["одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать"],
                ["десять", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят", "девяносто"],
                ["сто", "двести", "триста", "четыреста", "пятьсот", "шестьсот", "семьсот", "восемьсот", "девятьсот"],
-               ["тысяча", "две тысячи", "три тысячи", "четыре тысячи", "пять тысячь", "шесть тысячь", "семь тысячь", "восемь тысячь", "девять тысячь"]]
+               ["тысяча", "две тысячи", "три тысячи", "четыре тысячи", "пять тысячь", "шесть тысячь", "семь тысячь", "восемь тысячь", "девять тысячь"],
+               ["одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать"]]
+
+    flag = True
 
     def russian_unites(units: int) -> str:
-        if units == 0:
+        if units == 0 or not flag:
             return ""
-        for number_of_unites in range(units):
-            number_of_unites += units
-            if number_of_unites <= 9:
-                return numbers[0][number_of_unites - 1]
+        else:
+            return numbers[0][units - 1]
 
     def russian_dozens(dozen: int, units: int) -> str:
         if dozen == 0:
             return ""
-        for number_of_dozens in range(dozen):
-            number_of_dozens += dozen
-            if 1 <= number_of_dozens <= 9:
-                if number_of_dozens == 1 and units == 0:
-                    return numbers[2][0]
-                elif number_of_dozens == 1 and units >= 0:
-                    return numbers[1][number_of_dozens - 1]
-                else:
-                    return numbers[2][number_of_dozens - 1]
-    #
+        if 1 <= dozen <= 9:
+            if dozen == 1 and units == 0:
+                return numbers[2][0]
+            elif dozen == 1 and units >= 0:
+                flag = False
+                return numbers[1][dozen - 1]
+            else:
+                return numbers[2][dozen - 1]
+
     # def russian_hundreds(hundred: int) -> str:
+    #     if hundred == 0:
+    #         return ""
     #     for number_of_hundreds in range(hundred):
     #         number_of_hundreds += hundred
     #         if 1 <= number_of_hundreds <= 9:
     #             return numbers[3][number_of_hundreds - 1]
     #
     # def russia_thousands(thousand: int) -> str:
+    #     if thousand == 0:
+    #         return "тысяч"
     #     for number_of_thousand in range(thousand):
     #         number_of_thousand += thousand
     #         if 1 <= number_of_thousand <= 9:
     #             return numbers[4][number_of_thousand - 1]
+    a = '102'
+    result = []
+    for (index, digit) in enumerate(str(n)):
+        int_digit = int(digit)
+        list_index = (len(str(n)) - 1 - index) % 4
+        list_index2 = len(str(n)) - 1 - index
+        if list_index == 3:
+            flag = True
+        if int_digit == 0 or not flag:
+            continue
+        elif list_index == 1 and int_digit == 1 and str(n)[index + 1] != 0:
+            result.append(numbers[4][int(str(n)[index + 1]) - 1])
+            flag = False
+        elif list_index2 == 4:
+            result.append(numbers[list_index2][int_digit - 1])
+        else:
+            result.append(numbers[list_index][int_digit - 1])
 
-    return russian_dozens(n % 1000 % 100 // 10, n % 1000 % 100 % 10) + russian_unites(n % 1000 % 100 % 10)
+    return " ".join(result)
