3cb05dda76405bb325fa7e5857cb3a724dafda55
Ukropchick <ukropcikkaneev@yandex.ru>
2021-11-20 16:20:04 +0300

    absolute all

--- Lesson_8.py
+++ Lesson_8.py
@@ -38,60 +38,35 @@ def russian(n: int) -> str:
     numbers = [["один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"],
                ["десять", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят", "девяносто"],
                ["сто", "двести", "триста", "четыреста", "пятьсот", "шестьсот", "семьсот", "восемьсот", "девятьсот"],
-               ["тысяча", "две тысячи", "три тысячи", "четыре тысячи", "пять тысячь", "шесть тысячь", "семь тысячь", "восемь тысячь", "девять тысячь"],
                ["одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать"]]
 
-    flag = True
+    is_number_complex = False
 
-    def russian_unites(units: int) -> str:
-        if units == 0 or not flag:
-            return ""
-        else:
-            return numbers[0][units - 1]
-
-    def russian_dozens(dozen: int, units: int) -> str:
-        if dozen == 0:
-            return ""
-        if 1 <= dozen <= 9:
-            if dozen == 1 and units == 0:
-                return numbers[2][0]
-            elif dozen == 1 and units >= 0:
-                flag = False
-                return numbers[1][dozen - 1]
-            else:
-                return numbers[2][dozen - 1]
-
-    # def russian_hundreds(hundred: int) -> str:
-    #     if hundred == 0:
-    #         return ""
-    #     for number_of_hundreds in range(hundred):
-    #         number_of_hundreds += hundred
-    #         if 1 <= number_of_hundreds <= 9:
-    #             return numbers[3][number_of_hundreds - 1]
-    #
-    # def russia_thousands(thousand: int) -> str:
-    #     if thousand == 0:
-    #         return "тысяч"
-    #     for number_of_thousand in range(thousand):
-    #         number_of_thousand += thousand
-    #         if 1 <= number_of_thousand <= 9:
-    #             return numbers[4][number_of_thousand - 1]
-    a = '102'
     result = []
     for (index, digit) in enumerate(str(n)):
         int_digit = int(digit)
-        list_index = (len(str(n)) - 1 - index) % 4
-        list_index2 = len(str(n)) - 1 - index
+        list_index = len(str(n)) - 1 - index
         if list_index == 3:
-            flag = True
-        if int_digit == 0 or not flag:
+            if is_number_complex:
+                result.append("тысяч")
+            else:
+                if int_digit == 0:
+                    result.append("тысяч")
+                elif int_digit == 1:
+                    result.append("одна тысяча")
+                elif int_digit == 2:
+                    result.append("две тысячи")
+                elif int_digit <= 4:
+                    result.append(numbers[list_index % 3][int_digit - 1] + " тысячи")
+                elif int_digit <= 9:
+                    result.append(numbers[list_index % 3][int_digit - 1] + " тысяч")
+            is_number_complex = False # чтобы проверить несколько раз число
+        elif int_digit == 0 or is_number_complex:
             continue
-        elif list_index == 1 and int_digit == 1 and str(n)[index + 1] != 0:
-            result.append(numbers[4][int(str(n)[index + 1]) - 1])
-            flag = False
-        elif list_index2 == 4:
-            result.append(numbers[list_index2][int_digit - 1])
+        elif list_index % 3 == 1 and int_digit == 1 and int(str(n)[index + 1]) != 0:
+            result.append(numbers[3][int(str(n)[index + 1]) - 1])
+            is_number_complex = True
         else:
-            result.append(numbers[list_index][int_digit - 1])
+            result.append(numbers[list_index % 3][int_digit - 1])
 
     return " ".join(result)
