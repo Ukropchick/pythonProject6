680e1d41e747f9165d55c7d736e7f38fb0c7ab9d
Ukropchick <ukropcikkaneev@yandex.ru>
2021-12-03 19:27:08 +0300

    find_cheapest_stuff extract_repeats

--- Lesson_9.py
+++ Lesson_9.py
@@ -18,7 +18,28 @@
 
 
 def merge_phone_books(dict_a: dict, dict_b: dict) -> dict:
-    """TODO"""
+    # join = {}
+    #
+    # for key in dict_a:
+    #     value = dict_a[key]
+    #     if key in dict_b:
+    #         join[key] = []
+    #         if value == dict_b[key]:
+    #             join[key].append(value)
+    #     elif not (key in dict_b):
+    #         join[key] = value
+    #
+    # for key2 in dict_b:
+    #     value2 = dict_b[key2]
+    #     if key2 in dict_a:
+    #         if value2 == dict_a[key2]:
+    #             join[key] += value2
+    #         else:
+    #             join[key] += value2
+    #     elif not (key2 in dict_a):
+    #         join[key] = value2
+
+    return join
 
 
 """
@@ -39,7 +60,21 @@ def merge_phone_books(dict_a: dict, dict_b: dict) -> dict:
 
 
 def find_cheapest_stuff(stuff: dict, kind: str) -> str:
-    """TODO"""
+    keys = []
+    names = []
+    prices = []
+
+    for key in stuff:
+        keys.append(key)
+        name = stuff[key][0]
+        names.append(name)
+        price = stuff[key][1]
+        prices.append(price)
+    if names[0] == kind or names[1] == kind:
+        if prices[0] < prices[1]:
+            return keys[0]
+        else:
+            return keys[1]
 
 
 """
@@ -51,10 +86,22 @@ def find_cheapest_stuff(stuff: dict, kind: str) -> str:
  * не следует.
 
  Например:
- *   extract_repeats(["a", "b", "a"] -> {"a": 2}
+ *   extract_repeats(["a", "b", "a"]) -> {"a": 2}
  
 """
 
 
 def extract_repeats(input_list: list) -> dict:
-    """TODO"""
+    repeat = {}
+
+    if len(input_list) != 0:
+        for keys in input_list:
+            value = keys
+            if input_list.count(value) > 1:
+                repeat[value] = input_list.count(value)
+            else:
+                return repeat
+    else:
+        return repeat
+
+
