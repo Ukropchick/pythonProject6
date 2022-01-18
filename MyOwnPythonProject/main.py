91c7028b1d2c3263ef19832f964c1a0294f7f724
Ukropchick <ukropcikkaneev@yandex.ru>
2022-01-18 13:53:27 +0300

    class and file

--- MyOwnPythonProject/main.py
+++ MyOwnPythonProject/main.py
@@ -1,17 +1,18 @@
 from pyowm import *
 
-
-def weather_report():
-    city = input("Введите название города: ")
-    own = OWM('ecc37d11852cb8b06c7f97ee0a1bb259')
-    mgr = own.weather_manager()
-
-    observation = mgr.weather_at_place(city)
-    w = observation.weather
-
-    return w
-
-
-print(weather_report())
-
-'''Это тест'''
+sqs = [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+print(sqs[1::4])
+# def weather_report():
+#     city = input("Введите название города: ")
+#     own = OWM('ecc37d11852cb8b06c7f97ee0a1bb259')
+#     mgr = own.weather_manager()
+#
+#     observation = mgr.weather_at_place(city)
+#     w = observation.weather
+#
+#     return w
+#
+#
+# print(weather_report())
+#
+# '''Это тест'''
